4717e2cab62fc1df87aa1d8da34dc00603f06d82
Subzero6671 <43244190+Subzero6671@users.noreply.github.com>
2019-11-02 22:42:59 -0400

    gg
    .

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    //Fields
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float random;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition()
+    }
+}
